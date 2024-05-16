e75db6cdae8e362b90a8decd6d2065ecfaca4f08
Che <profesor76@hotmail.com>
2024-05-16 14:43:14 -0600

    160524

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -64,60 +64,80 @@ $(document).on("click", ".boton-nuevo-acceso", function () {
 
 $(document).on("click", ".boton-iniciar-sesion", function () {
 
-    const modelo = {
-        idEmpleado: _modeloEmpleado.idEmpleado,
-        nombreCompleto: $("#txtNombreCompleto").val(),
-        refDepartamento: {
-            idDepartamento: $("#cboDepartamento").val()
-        },
-        sueldo: $("#txtSueldo").val(),
-        fechaContrato: $("#txtFechaContrato").val()
-    }
-
-
-    if (_modeloEmpleado.idEmpleado == 0) {
-
-        fetch("/Home/guardarEmpleado", {
-            method: "POST",
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            body: JSON.stringify(modelo)
+    fetch("/Home/listaUsuarios")
+        .then(response => {
+            return response.ok ? response.json() : Promise.reject(response)
         })
-            .then(response => {
-                return response.ok ? response.json() : Promise.reject(response)
-            })
-            .then(responseJson => {
+        .then(responseJson => {
 
-                if (responseJson.valor) {
-                    $("#modalEmpleado").modal("hide");
-                    Swal.fire("Listo!", "Empleado fue creado", "success");
-                    MostrarEmpleados();
-                }
-                else
-                    Swal.fire("Lo sentimos", "No se puedo crear", "error");
-            })
+            if (responseJson.length > 0) {
+                responseJson.forEach((item) => {
 
-    } else {
+                    //$("#cboTipoPropiedad").append(
+                    //    $("<option>").val(item.idTipoPropiedad).text(item.nombre)
+                    //)
 
-        fetch("/Home/editarEmpleado", {
-            method: "PUT",
-            headers: { "Content-Type": "application/json; charset=utf-8" },
-            body: JSON.stringify(modelo)
+                    alert("Usuario loggeado");
+
+                })
+            }
+
         })
-            .then(response => {
-                return response.ok ? response.json() : Promise.reject(response)
-            })
-            .then(responseJson => {
 
-                if (responseJson.valor) {
-                    $("#modalEmpleado").modal("hide");
-                    Swal.fire("Listo!", "Empleado fue actualizado", "success");
-                    MostrarEmpleados();
-                }
-                else
-                    Swal.fire("Lo sentimos", "No se puedo actualizar", "error");
-            })
+    //const modelo = {
+    //    idEmpleado: _modeloEmpleado.idEmpleado,
+    //    nombreCompleto: $("#txtNombreCompleto").val(),
+    //    refDepartamento: {
+    //        idDepartamento: $("#cboDepartamento").val()
+    //    },
+    //    sueldo: $("#txtSueldo").val(),
+    //    fechaContrato: $("#txtFechaContrato").val()
+    //}
 
-    }
+
+    //if (_modeloEmpleado.idEmpleado == 0) {
+
+    //    fetch("/Home/guardarEmpleado", {
+    //        method: "POST",
+    //        headers: { "Content-Type": "application/json; charset=utf-8" },
+    //        body: JSON.stringify(modelo)
+    //    })
+    //        .then(response => {
+    //            return response.ok ? response.json() : Promise.reject(response)
+    //        })
+    //        .then(responseJson => {
+
+    //            if (responseJson.valor) {
+    //                $("#modalEmpleado").modal("hide");
+    //                Swal.fire("Listo!", "Empleado fue creado", "success");
+    //                MostrarEmpleados();
+    //            }
+    //            else
+    //                Swal.fire("Lo sentimos", "No se puedo crear", "error");
+    //        })
+
+    //} else {
+
+    //    fetch("/Home/editarEmpleado", {
+    //        method: "PUT",
+    //        headers: { "Content-Type": "application/json; charset=utf-8" },
+    //        body: JSON.stringify(modelo)
+    //    })
+    //        .then(response => {
+    //            return response.ok ? response.json() : Promise.reject(response)
+    //        })
+    //        .then(responseJson => {
+
+    //            if (responseJson.valor) {
+    //                $("#modalEmpleado").modal("hide");
+    //                Swal.fire("Listo!", "Empleado fue actualizado", "success");
+    //                MostrarEmpleados();
+    //            }
+    //            else
+    //                Swal.fire("Lo sentimos", "No se puedo actualizar", "error");
+    //        })
+
+    //}
 
 
 })
