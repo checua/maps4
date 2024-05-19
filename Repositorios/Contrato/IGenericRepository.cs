1065b31f2f60dac1f05f85c0c5de403612210bc4
Che <profesor76@hotmail.com>
2024-05-18 23:49:08 -0600

    180524

--- Repositorios/Contrato/IGenericRepository.cs
+++ Repositorios/Contrato/IGenericRepository.cs
@@ -5,15 +5,8 @@ namespace maps4.Repositorios.Contrato
     public interface IGenericRepository<T> where T : class
     {
         Task<List<T>> Lista();
-        Task<bool> Guardar(T modelo);
-        Task<bool> Editar(T modelo);
-        Task<bool> Eliminar(int id);
-    }
-
-    public interface IUsuarioService
-    {
-        Task<Usuario> GetUsuario(string correo, string clave);
-        Task<Usuario> SaveUsuario(Usuario modelo);
-
+        //Task<bool> Guardar(T modelo);
+        //Task<bool> Editar(T modelo);
+        //Task<bool> Eliminar(int id);
     }
 }
