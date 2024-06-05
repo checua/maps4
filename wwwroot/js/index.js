95f8eb378bd2ec5e560637a7402b05f00f24b019
Che <profesor76@hotmail.com>
2024-06-05 11:58:13 -0600

    05 06 24

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -7,6 +7,158 @@
 }
 document.addEventListener("DOMContentLoaded", function () {
 
+    var latLng = new google.maps.LatLng(24.02, -104.62);
+    var opciones = {
+        center: latLng,
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.roadmap,
+        disableDefaultUI: true
+    };
+
+    var map = new google.maps.Map(document.getElementById('map_canvas'), opciones);
+    geocoder = new google.maps.Geocoder();
+    infowindow = new google.maps.InfoWindow();
+
+    fetch("/Home/listaInmuebles")
+        .then(response => {
+            return response.ok ? response.json() : Promise.reject(response)
+        })
+        .then(responseJson => {
+
+            if (responseJson.length > 0) {
+
+                responseJson.forEach((item) => {
+                    switch (item.idTipo) {
+                        case 0:
+                            imageIcon = "images/icon/casa_che.png";
+                            break;
+                        case 1:
+                            imageIcon = "images/icon/casa_che.png";
+                            break;
+                        case 2:
+                            imageIcon = "images/icon/casa.png";
+                            break;
+                        case 3:
+                            imageIcon = "images/icon/apartment1.png";
+                            break;
+                        case 4:
+                            imageIcon = "images/icon/apartment2.png";
+                            break;
+                        case 5:
+                            imageIcon = "images/icon/terreno1b.png";
+                            break;
+                        case 6:
+                            imageIcon = "images/icon/terreno2.png";
+                            break
+                        case 7:
+                            imageIcon = "images/icon/local.png";
+                            break
+                        case 8:
+                            imageIcon = "images/icon/local2.png";
+                            break
+                        case 9:
+                            imageIcon = "images/icon/building.png";
+                            break
+                        case 10:
+                            imageIcon = "images/icon/building2.png";
+                            break
+                        case 11:
+                            imageIcon = "images/icon/montacargas2.png";
+                            break
+                        case 12:
+                            imageIcon = "images/icon/montacargas.png";
+                            break
+                        case 13:
+                            imageIcon = "images/icon/desk.png";
+                            break
+                        case 14:
+                            imageIcon = "images/icon/desk2.png";
+                            break
+                        case 15:
+                            imageIcon = "images/icon/tractor2.png";
+                            break
+                        case 16:
+                            imageIcon = "images/icon/tractor3.png";
+                            break
+                    }
+
+                    var latLng = new google.maps.LatLng(item.lat, item.lng);
+                    new google.maps.Marker({
+                        position: latLng,
+                        map,
+                        title: String(item.idTipo),
+                        icon: {
+                            url: imageIcon,
+                            scaledSize: new google.maps.Size(32, 32)
+                        },
+                    });
+                })
+                map.setCenter(latLng);
+            }
+        })
+
+    google.maps.event.addListener(map, 'mousedown', function (e) {
+
+        var latLng = e.latLng;
+
+        marker = new google.maps.Marker({
+            position: latLng,
+            map: map,
+            draggable: false
+        });
+
+        if (mousedUp === false) {
+            var log = document.getElementById("lnkAcceso").innerText;
+
+            //if (log == "Iniciar Sesión")
+            //    alert("Ingresa para poder registrar propiedades");
+            //else {
+            //    alert("Bienvenido");
+            //}
+
+
+            if (log == "Iniciar Sesión")
+                alert("Ingresa para poder registrar propiedades");
+            else {
+                /*alert(log);*/
+                geocoder.geocode({ 'latLng': event.latLng }, function (results, status) {
+
+                    /*alert("Geocode");*/
+                    if (status == google.maps.GeocoderStatus.OK) {
+
+                        alert("Status ok");
+
+                        //if (results[0]) {
+                        //    alert("No result 0");
+                        //    ////            document.getElementById("hfCoordenadas").value = results[0].geometry.location;
+                        //    ////            document.getElementById("hfLat").value = event.latLng.lat().toFixed(6);
+                        //    ////            document.getElementById("hfLng").value = event.latLng.lng().toFixed(6);
+                        //    ////            document.getElementById("hfDireccion").value = results[0].formatted_address;
+
+                        //    ////            marker = new google.maps.Marker({
+                        //    ////                position: event.latLng,
+                        //    ////                map: map
+                        //    ////            });
+
+                        //    ////            GetCode2();
+
+                        //} else {
+                        //    ////            document.getElementById('geocoding').innerHTML =
+                        //    ////                'No se encontraron resultados';
+                        //    alert("No result 0");
+                        //}
+
+                    }
+                    else {
+                        document.getElementById("lnkAcceso").innerHTML = 'Geocodificación  ha fallado debido a: ' + status;
+                    }
+                });
+            }
+
+        }
+
+    });
+
     //MostrarEmpleados();
 
     $.ajax({
@@ -95,9 +247,7 @@ $(document).on("click", ".boton-iniciar-sesion", function () {
             }
         })
 })
-                return response.ok ? response.json() : Promise.reject(response)
-            })
-            .then(responseJson => {
+
 
 //$(document).on("click", ".boton-iniciar-sesion", function () {
 //    fetch("/Inicio/IniciarSesion?correo=" + $("#correo").val() + "&contra=" + $("#contra").val())
@@ -118,7 +268,3 @@ $(document).on("click", ".boton-iniciar-sesion", function () {
 //        })
 //})
 
-    }
-
-
-})
