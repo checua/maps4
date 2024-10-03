8872b09163db2ff143bfaccfb26b7d1cbcb36793
Che <profesor76@hotmail.com>
2024-10-03 11:25:07 -0600

    031024 NuGet ok

---
+++ Views/Inicio/compartir.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace maps4.wwwroot
+{
+    public class compartirModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
