b35f347cbc3a84e592163160387fde389cc86f1b
checua <78944400+checua@users.noreply.github.com>
2026-08-18 14:39:18 -0600

    Require authenticated actor for property deletion

--- Repositorios/Contrato/IInmuebleServicio.cs
+++ Repositorios/Contrato/IInmuebleServicio.cs
@@ -4,17 +4,12 @@ namespace maps4.Repositorios.Contrato
 {
     public interface IInmuebleServicio<T> where T : class
     {
-        //Task<List<T>> GetInmuebles();
-
-        Task<Inmueble> SaveInmueble(Inmueble modelo);//, int files, string correo);
+        Task<Inmueble> SaveInmueble(Inmueble modelo);
 
         Task<bool> UpdateInmueble(Inmueble modelo);
 
-        Task<bool> EliminarInmueble(int idInmueble);
-
+        Task<bool> EliminarInmueble(int idInmueble, string correoAutenticado);
 
         Task<List<T>> GetInmuebleById(int idInmueble);
-
-
     }
 }
