4786c79e3180029ba3adec8a4e5aada020a123ba
checua <78944400+checua@users.noreply.github.com>
2026-08-20 11:55:19 -0600

    Add draft creation request model

---
+++ Models/CrearBorradorRequest.cs
@@ -0,0 +1,9 @@
+namespace maps4.Models
+{
+    public class CrearBorradorRequest
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+        public int IdTipo { get; set; }
+    }
+}
