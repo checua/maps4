76518ff5fdace21218d2283a1abaa84b75d2eb58
checua <78944400+checua@users.noreply.github.com>
2026-08-21 14:40:06 -0600

    Add marketplace amenity filter model

---
+++ Models/AmenidadFiltroViewModel.cs
@@ -0,0 +1,10 @@
+namespace maps4.Models
+{
+    public class AmenidadFiltroViewModel
+    {
+        public string Codigo { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Grupo { get; set; } = string.Empty;
+        public int Orden { get; set; }
+    }
+}
