2394e260c8512488ff64729e4b30ffce6ada120f
checua <78944400+checua@users.noreply.github.com>
2026-08-23 19:39:16 -0600

    Register Radar matching service

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using maps4.Models;
 using maps4.Repositorios.Contrato;
 using maps4.Repositorios.Implementacion;
+using maps4.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
@@ -21,6 +22,7 @@ builder.Services.AddScoped<IGenericRepository<Inmueble>, InmuebleRepository>();
 builder.Services.AddScoped<IInmuebleServicio<Inmueble>, InmuebleRegistroRepository>();
 builder.Services.AddScoped<IComentarioService, ComentarioService>();
 builder.Services.AddScoped<IInventarioRepository, InventarioRepository>();
+builder.Services.AddScoped<IRadarMatchingService, RadarMatchingService>();
 builder.Services.AddScoped<IBorradorInmuebleRepository, BorradorInmuebleRepository>();
 builder.Services.AddScoped<IPublicacionBorradorRepository, PublicacionBorradorRepository>();
 builder.Services.AddScoped<IInmuebleFotoRepository, InmuebleFotoRepository>();
