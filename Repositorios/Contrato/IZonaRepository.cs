5cb210c2532a144999d19059a96c352bae2e63a5
checua <78944400+checua@users.noreply.github.com>
2026-08-25 11:26:52 -0600

    Add zone repository contract

---
+++ Repositorios/Contrato/IZonaRepository.cs
@@ -0,0 +1,11 @@
+using maps4.Models;
+
+namespace maps4.Repositorios.Contrato
+{
+    public interface IZonaRepository
+    {
+        Task<List<ZonaResumenViewModel>> ListarAsync(string correo);
+        Task<ZonaEdicionViewModel?> ObtenerAsync(string correo, int idZona);
+        Task<int> GuardarAsync(string correo, ZonaGuardarRequest request);
+    }
+}
